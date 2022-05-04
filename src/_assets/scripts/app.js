5feae1ec25506db3e88edb9f3e0d969a47323ac0
Lars Knoke <l.knoke@colorplus.de>
2022-05-04 09:13:55 +0200

    vorhang

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -1,16 +1,49 @@
 console.log(`I was loaded at ${Date(Date.now()).toString()}`);
 
+const mainWrapper = document.querySelector("#main");
+const contentWrapper = document.querySelector(".content");
 const leistungTrigger = document.querySelector(".leistung-trigger");
-const gras = document.querySelector(".gras");
+const grasLeft = document.querySelector(".gras-left");
+const grasRight = document.querySelector(".gras-right");
 const hand = document.querySelector(".hand");
 
 leistungTrigger.addEventListener("mouseover", () => {
-	gras.classList.add("-translate-x-12");
-	hand.classList.add("opacity-100");
-	hand.classList.add("translate-x-16");
+	let open = mainWrapper.classList.contains("open");
+
+	if (!open) {
+		grasLeft.classList.add("-translate-x-12");
+		hand.classList.add("opacity-100");
+		hand.classList.add("translate-x-16");
+	}
 });
 leistungTrigger.addEventListener("mouseout", () => {
-	gras.classList.remove("-translate-x-12");
+	grasLeft.classList.remove("-translate-x-12");
 	hand.classList.remove("opacity-100");
 	hand.classList.remove("translate-x-16");
 });
+
+leistungTrigger.addEventListener("click", (e) => {
+	localStorage.setItem("open", "true");
+	const is_root = location.pathname == "/";
+	if (is_root) {
+		localStorage.setItem("isRoot", "true");
+	} else {
+		localStorage.setItem("isRoot", "false");
+	}
+
+	// e.preventDefault();
+	// mainWrapper.classList.add("open");
+	// grasLeft.classList.remove("-translate-x-12");
+	// grasLeft.classList.add("left-[0%]");
+	// grasLeft.classList.add("w-[10%]");
+	// grasRight.classList.add("right-[0%]");
+	// grasRight.classList.add("w-[10%]");
+	// hand.classList.remove("opacity-100");
+	// hand.classList.remove("translate-x-16");
+});
+
+setTimeout(() => {
+	if (localStorage.getItem("open") == "true") {
+		contentWrapper.classList.add("open");
+	}
+}, 1);
